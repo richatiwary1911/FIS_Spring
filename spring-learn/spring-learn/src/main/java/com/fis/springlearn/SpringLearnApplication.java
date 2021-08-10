d16383b8e5bfb820ef3859997a5bf43e266a1610
Richa Tiwary <richatiwary1911@gmail.com>
2021-08-10 16:49:54 +0530

    Creating layers using Annotations

--- spring-learn/spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -9,11 +9,13 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.fis.springlearn.bean.Employee;
+import com.fis.springlearn.controller.EmployeeController;
 
 import java.util.ArrayList;
 
@@ -23,11 +25,22 @@ public class SpringLearnApplication {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
+		//SpringApplication.run(SpringLearnApplication.class, args);
 		//displayDate();
 		//displayCountry();
 		//displayCountries();
-		displayEmployee();
+		//displayEmployee();
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(EmployeeController.class);
+		displayEmployeeControllerAnnotation(applicationContext);
+	}
+	
+	public static void displayEmployeeControllerAnnotation(ApplicationContext applicationContext)
+	{
+		LOGGER.info("START");
+		EmployeeController employeeController = applicationContext.getBean("employeeController", EmployeeController.class);
+		LOGGER.debug("EmployeeController : {}", employeeController);
+		LOGGER.info("END");
+
 	}
 	
 	static void displayEmployee()
--- spring-learn/spring-learn/src/main/java/com/fis/springlearn/dao/EmployeeDao.java
+++ spring-learn/spring-learn/src/main/java/com/fis/springlearn/dao/EmployeeDao.java
@@ -2,9 +2,11 @@ package com.fis.springlearn.dao;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import com.fis.springlearn.controller.EmployeeController;
 
+@Component
 public class EmployeeDao {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
